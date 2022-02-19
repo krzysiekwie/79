5683f5fc563849aca2046fa7ca20d328532ddcad
krzysiekwie@gmail.com <krzysiekwie@gmail.com>
2022-02-19 18:06:30 +0100

    navbar edited

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,19 +1,55 @@
 <template>
   <nav class="fixed p-2 bg-gray-100 w-screen shadow-lg z-50">
     <ul class="flex border-b">
-      <li class="-mb-px mr-1">
-        <router-link
-          class="bg-gray-100 inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
-          to="/"
-          >Home</router-link
+      <li class="mr-1">
+        <a
+          class="
+            bg-gray-100
+            inline-block
+            py-2
+            px-4
+            text-blue-500
+            hover:text-blue-800
+            font-semibold
+          "
+          href="https://krzysiekwie.github.io/"
+          >krzysiekwie.github.io</a
         >
       </li>
-      <!-- <li class="mr-1">
-            <a class="bg-gray-100 inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">Tab</a>
-            </li>
-            <li class="mr-1">
-            <a class="bg-gray-100 inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">Tab</a>
-            </li>
+      <li class="-mb-px mr-1">
+        <router-link
+          class="
+            bg-gray-100
+            inline-block
+            border-l border-t border-r
+            rounded-t
+            py-2
+            px-4
+            text-blue-700
+            font-semibold
+          "
+          to="/"
+          >Projects</router-link
+        >
+      </li>
+      <li class="mr-1">
+        <a
+          class="
+            bg-gray-100
+            inline-block
+            py-2
+            px-4
+            text-blue-500
+            hover:text-blue-800
+            font-semibold
+          "
+          href="https://krzysiekwie.github.io/hugowie/"
+          >Notes</a
+        >
+      </li>
+
+      <!-- 
+            
             <li class="mr-1">
             <a class="bg-gray-100 inline-block py-2 px-4 text-gray-400 font-semibold" href="#">Tab</a>
             </li>
@@ -30,7 +66,7 @@ export default {
   name: "MainNavbar",
   data() {
     return {};
-  }
+  },
 };
 </script>
 
